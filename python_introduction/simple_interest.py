eef868e361410db5ea78f8ff61c0658f16fd1447
Alex Alexander <raregemalex@gmail.com>
2025-05-22 04:07:52 +0100

    Update simple interest calculator

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,12 +1,12 @@
 # simple_interest.py
 
 # Define principal, rate and time
-principal = 1000 # $1000
-rate = 0.05  # 5%
-time = 3  # 3 years
+principal = P = 1000 # $1000
+rate = R = 0.05  # 5%
+time = T = 3  # 3 years
 
 # Calculate simple interest
-interest = (principal * rate * time)
+interest = I = (P * R * T)
 
 # Print the result
 print(f"The simple interest is: {interest}.")
